f5d55cc43569adc2d319e0122f714d26b7251ef6
Vineeth Bharadwaj <vineethpbharadwaj@gmail.com>
2018-07-05 08:12:20 +0200

    deep learning & model selection update

--- Model Selection/Py_Kfold_cv.py
+++ Model Selection/Py_Kfold_cv.py
@@ -1,5 +1,9 @@
-# Kernel SVM
+"""
+@author: Vineeth
+"""
+# k-Fold Cross Validation
 
+# Kernel SVM
 # Importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
@@ -11,7 +15,7 @@ X = dataset.iloc[:, [2, 3]].values
 y = dataset.iloc[:, 4].values
 
 # Splitting the dataset into the Training set and Test set
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
 
 # Feature Scaling
@@ -31,6 +35,16 @@ y_pred = classifier.predict(X_test)
 # Making the Confusion Matrix
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
+
+
+#Applying k-Fold cross validation
+from sklearn.model_selection import cross_val_score
+accuracies = cross_val_score(estimator = classifier, X = X_train,
+                             y = y_train, cv = 10)
+
+acc_mean = accuracies.mean()
+acc_std = accuracies.std()
+
 
 # Visualising the Training set results
 from matplotlib.colors import ListedColormap
--- Others/Py_kernel_PCA.py
+++ Others/Py_kernel_PCA.py
@@ -1,4 +1,8 @@
-# Kernel SVM
+"""
+@author: Vineeth
+"""
+
+# Kernel PCA
 
 # Importing the libraries
 import numpy as np
@@ -11,7 +15,7 @@ X = dataset.iloc[:, [2, 3]].values
 y = dataset.iloc[:, 4].values
 
 # Splitting the dataset into the Training set and Test set
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
 
 # Feature Scaling
@@ -20,9 +24,16 @@ sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
-# Fitting Kernel SVM to the Training set
-from sklearn.svm import SVC
-classifier = SVC(kernel = 'rbf', random_state = 0)
+# Applying Kernel PCA
+from sklearn.decomposition import KernelPCA
+k_pca = KernelPCA(n_components = 2, kernel = 'rbf')
+X_train = k_pca.fit_transform(X_train)
+X_test = k_pca.transform(X_test)
+
+
+# Fitting Logistic Regression to the Training set
+from sklearn.linear_model import LogisticRegression
+classifier = LogisticRegression(random_state = 0)
 classifier.fit(X_train, y_train)
 
 # Predicting the Test set results
@@ -44,7 +55,7 @@ plt.ylim(X2.min(), X2.max())
 for i, j in enumerate(np.unique(y_set)):
     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
                 c = ListedColormap(('red', 'green'))(i), label = j)
-plt.title('Kernel SVM (Training set)')
+plt.title('Logistic Regression (Training set)')
 plt.xlabel('Age')
 plt.ylabel('Estimated Salary')
 plt.legend()
@@ -62,7 +73,7 @@ plt.ylim(X2.min(), X2.max())
 for i, j in enumerate(np.unique(y_set)):
     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
                 c = ListedColormap(('red', 'green'))(i), label = j)
-plt.title('Kernel SVM (Test set)')
+plt.title('Logistic Regression (Test set)')
 plt.xlabel('Age')
 plt.ylabel('Estimated Salary')
 plt.legend()
